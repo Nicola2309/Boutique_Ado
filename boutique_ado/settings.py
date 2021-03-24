0e919eb7f80cdccb92f032350a290fe1c8939827
Nicola2309 <nicola.spigaglia@outlook.it>
2021-03-24 17:11:22 +0000

    Added webhook handler

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -53,6 +53,12 @@ INSTALLED_APPS = [
     'home',
     'products',
     'bag',
+    'checkout',
+
+    # import boostrap4 classes automatically, W-O-W.
+    # after the 'pip3 install django-crispy-forms' command
+    'crispy_forms',
+
 ]
 
 MIDDLEWARE = [
@@ -66,6 +72,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'boutique_ado.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -81,9 +89,14 @@ TEMPLATES = [
                 # required by allauth jimmy, it allows allauth-django
                 # to access HTTP request object in our templates.
                 'django.template.context_processors.request',
+                'django.template.context_processors.media',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
+            ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
             ],
         },
     },
@@ -192,3 +205,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
