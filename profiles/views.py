89c05d1f2b32493df4c2dbba892fc4227891b0ee
Nicola2309 <nicola.spigaglia@outlook.it>
2021-03-26 16:11:19 +0000

    Updated allauth templates and tested profiles

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-
+from .models import UserProfile
 
 def profile(request):
     """ Display user's profile matafakka """
 
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)
